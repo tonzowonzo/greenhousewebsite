b6852144cb1b506b6a249b1679f7d1d3d5a91fe7
Tim <35193581+tonzowonzo@users.noreply.github.com>
2018-06-30 23:55:23 +0200

    Add files via upload

--- greenhousewebsite/greenhousegame.js
+++ greenhousewebsite/greenhousegame.js
@@ -3,6 +3,10 @@
 const GREENHOUSE_SIZE_MULTIPLIER = 1;
 // Framerate.
 const FPS = 30;
+// Plant size in pixels.
+const PLANT_SIZE = 20;
+// Light radius from lamps in pixels.
+const LAMP_RADIUS = 80;
 
 // Variables
 /** @type {HTMLCanvasElement} */
@@ -12,36 +16,73 @@ var ctx = canv.getContext("2d");
 var currentMoney = 0;
 // How high is the current greenhouse in stories.
 var numberOfGreenhouseStories = 1;
-// Greenhouse dimensions (x and y).
-var greenhouseDimensions = {
-	width: 200 * GREENHOUSE_SIZE_MULTIPLIER,
-	height: 150 * GREENHOUSE_SIZE_MULTIPLIER
-}
-// Centre of the canvas.
-centreCoords = {
-	x: canv.width/2 - greenhouseDimensions.width/2,
-	y: canv.width/2 - greenhouseDimensions.height
-}
+
+// Number of plants in the greenhouse.
+var numPlants = 300;
+
+// Number of irrigators in the greenhouse.
+
+// Number of vents in the greenhouse.
+
+// Are the vents open or closed?
+
+// Number of solar panels in greenhouse.
+
+// Time of day (hour)
+var time = 0;
+var isNight = true;
+
 // Set up the game loop.
 setInterval(update, 1000 / FPS);
+	
 
-function update() {
-	// draw space
-	ctx.fillStyle = "#589ea5";
-	ctx.fillRect(0, 0, canv.width, canv.height);
+	function update() {
+		// Update time variable.
+		time += 1;
+		// Draw the background color, changes with day vs night.
+		if (time <= 120) {
+		ctx.fillStyle = "#ebecf2";
+		}
+		else if (time > 120 && time <= 240) {
+			ctx.fillStyle = "black";
+		}
+		else {
+			time = 0;
+		}
+		ctx.fillRect(0, 0, canv.width, canv.height);
+		
+		// Draw greenhouse shape.
+		ctx.rect(0, 0, canv.width, canv.height);
+		ctx.stroke();
 
-	// Draw greenhouse shape.
-	ctx.rect(centreCoords.x, centreCoords.y, greenhouseDimensions.width, greenhouseDimensions.height);
-	ctx.stroke();
-	// Draw plants.
+		// Draw plants.
+		var plantCol = 0;
+		var plantRow = 0;
+		for (var i=0; i<numPlants; i++) {
+			
+			if ((0 + PLANT_SIZE + PLANT_SIZE * plantCol * 2) < canv.width && plantRow < canv.height - PLANT_SIZE) {
+				ctx.fillStyle = "green";
+				ctx.beginPath();
+				ctx.arc(0 + PLANT_SIZE + PLANT_SIZE * plantCol * 2, plantRow + PLANT_SIZE, PLANT_SIZE, 0, 2*Math.PI);
+				ctx.stroke();
+				ctx.fill();
+				plantCol++;
+			}
+			else {
+				plantCol = 0;
+				plantRow+=PLANT_SIZE*4;
+			}
+		}
+		// Draw irrigators.
 
-	// Draw irrigators.
+		// Draw vents.
 
-	// Draw vents.
+		// Draw solar panels.
 
-	// Draw solar panels.
+		// Draw light and lighted up areas.
+		
+		// Draw shadows.
 
-	// Draw shadows.
 
 
 }
